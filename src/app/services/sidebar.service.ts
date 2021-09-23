de80676f58167b7e5a7b95d507df140f8bf5ed86
Martin Nicolas <nicolasfelipemartin@gmail.com>
2021-09-23 15:56:21 -0500

    Se agregan servicios y ajustes en el css del proyecto

---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      title: 'Dashboard!!',
+      icon: 'mdi mdi-gauge',
+      subMenu: [
+        {
+          title: 'Main',
+          url: '/'
+        },
+        {
+          title: 'Progressbar',
+          url: 'progress'
+        },
+        {
+          title: 'Gráficas',
+          url: 'grafica1'
+        }
+      ]
+    }
+  ]
+
+  constructor() { }
+}
